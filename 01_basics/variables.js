7a03d5c12ba9a42dac5c5dd9e9f69745a58e7e09
Vishal Singh <126417458+vshl2706@users.noreply.github.com>
2024-05-17 09:13:18 +0000

    Variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,6 @@
+const accountId = 12233445 
+// You cannot overwrite const variable
+let accountEmail = "vshl2706@gmail.com"
+var passWord = "12345"
+accountCity = "Jaipur"
+console.table([accountId, accountEmail, passWord, accountCity])
