9bc500937fd330b5dbd8f5290160fe3ffce7e033
Vishal Singh <126417458+vshl2706@users.noreply.github.com>
2024-05-17 12:41:27 +0000

    Data types

---
+++ 01_basics/03_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3) // We are using nodejs, not browser
+
+console.log(3 + 3);
+console.log("Vishal"); // Code readability should be high
+
+let name = "Vishal" //String data type;
+let age = 20 //intger data type
+let isLoggedIn = false //boolean data type
+
+// Number -> 2 to the power 53
+//bigint is used for bigger integer
+//string =? ""
+//boolean => true/false
+//null =? standalone value or represents epmty value
+//undefined => 
+//symbol => used mostly for uniqueness
+
+//object
+
+console.log(typeof "Vishal"); //-> string
+console.log(typeof age); //->number
+console.log(typeof null); //-> object
+console.log(typeof undefined) //->undefined
